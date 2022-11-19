0ec5b45c53c4beb6b1bddc260531ba87ec2009cd
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2022-11-19 20:47:24 +0800

    Adding the questions

--- Interesting_PythonProject.py
+++ Interesting_PythonProject.py
@@ -16,3 +16,27 @@ if answer.lower() == "central processing unit":
     score += 1
 else:
     print("Incorrect!")
+
+#Quesion 2
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print('*****  Correct!  *****\n')
+    score += 1
+else:
+    print("*****  Incorrect!  *****\n")
+
+#Quesion 3
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print('*****  Correct!  *****\n')
+    score += 1
+else:
+    print("*****  Incorrect!  *****\n")
+
+#Quesion 4
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print('*****  Correct!  *****\n')
+    score += 1
+else:
+    print("*****  Incorrect!  *****\n")
