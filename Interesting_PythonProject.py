fcda817273bc86d6d6f050612902af477377a984
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2022-11-19 20:28:14 +0800

    Ask the user

--- Interesting_PythonProject.py
+++ Interesting_PythonProject.py
@@ -2,3 +2,6 @@ print("_________________________________________________________________________
 print("This is the reference video link: https://www.youtube.com/watch?v=DLn3jOsNRVE&t=185s")
 print("\n____________________________________________________________________________________")
 
+#ask the user
+print("\n|======= Welcome to my Quiz Game =======|")
+playing = input("\nDo you want to play? ")
