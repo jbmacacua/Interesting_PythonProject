ca8c6701afe2c5bdd8ae936ac243057c1940acb9
Jonas Brian R. Macacua <jbrmacacua03@gmail.com>
2022-11-19 20:38:17 +0800

    If playing = yes

--- Interesting_PythonProject.py
+++ Interesting_PythonProject.py
@@ -5,3 +5,14 @@ print("\n_______________________________________________________________________
 #ask the user
 print("\n|======= Welcome to my Quiz Game =======|")
 playing = input("\nDo you want to play? ")
+
+#if playing = yes
+print("\nOkay! Let's play :)")
+
+#Question 1
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
